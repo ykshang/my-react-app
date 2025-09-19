07546deb06719ac9f3ba9f088831570c7df7e18a
kangkang shang <535634238@qq.com>
2025-09-19 19:54:03 +0800

    feat: 尝试添加我的第一个jsx组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Profile />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/Profile.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <img
+      src="src/assets/react.svg"
+      alt="Katherine Johnson"
+    />
+  )
+}
